6fafb666cc1888cd71a6ed7c8e4b58e7b95dd4a7
sunnysmilen <davidsalnicole@gmail.com>
2025-06-18 15:52:59 +0200

    feat: creation of homepage

---
+++ client/src/pages/Homepage.tsx
@@ -0,0 +1,21 @@
+export default function Homepage(){
+    return(
+        <>
+    <h1>Cabinet de recrutement informatique</h1>
+    <img src="" alt="Développeur qui regarde un écran avec du code" />
+    <p>Nous cultivons les liens que nous établissons, les faisant grandir sur le long terme.
+         En tant que membre actif de l’écosystème tech local, nous nous appuyons sur un réseau riche en expériences et en expertises. 
+        Cela nous permet de tisser des liens à la fois pertinents et vertueux.</p>
+    <h3>Les liens les plus durables.</h3>
+    <section>
+    <ul>
+        <li>Cybersécurité</li>
+        <li>Informatique & IT</li>
+        <li>Date & IA</li>
+    </ul>
+    </section>
+    <h3>Externatic, plus que du recrutement.</h3>
+    <img src="" alt="" />
+    </>
+)
+}
