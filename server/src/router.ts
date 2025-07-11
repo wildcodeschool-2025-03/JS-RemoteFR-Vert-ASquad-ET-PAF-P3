bb8930ef860a4477a1100675d6cb22fd115b2624
sunnysmilen <davidsalnicole@gmail.com>
2025-07-11 14:11:13 +0200

    feat: added route for connexion and inscription pages

--- server/src/router.ts
+++ server/src/router.ts
@@ -23,12 +23,25 @@ router.put("/home/:id", homepageActions.edit);
 router.delete("/home/:id", homepageActions.destroy);
 
 /* ************************************************************************* */
+import authActions from "./modules/auth/authActions";
 import offersActions from "./modules/offers/offersActions";
+import userActions from "./modules/user/userActions";
 
 router.get("/offers", offersActions.browse);
 router.get("/offers/:id", offersActions.read);
 router.post("/offers", offersActions.add);
 router.put("/offer/:id", offersActions.edit);
 router.delete("/offer/:id", offersActions.destroy);
+
+// router.get("/inscription", userActions.browse);
+// router.get("/inscription/:id", userActions.read);
+router.post("/inscription", authActions.hashPassword, userActions.add);
+
+router.post("/connexion", authActions.login);
+
+router.get("/connexion", userActions.add);
+// router.get("/connexion/:id", userActions.read)
+// router.put("/connexion/:id", userActions.edit);
+// router.delete("/connexion/:id", userActions.destroy);
 
 export default router;
