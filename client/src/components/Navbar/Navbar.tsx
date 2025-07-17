f143a759becb707b6bae2bef190ddd9a4dbed56d
sunnysmilen <davidsalnicole@gmail.com>
2025-07-17 09:42:21 +0200

    feat: added automatique closing of navbar that was before

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -110,7 +110,8 @@ const Navbar: React.FC = () => {
                 <button
                   type="button"
                   className="mobile-btn-login"
-                  onClick={() => navigate("/login")}
+                  onClick={() => {navigate("/login"); setIsMenuOpen(false);}}
+                
                 >
                   Se connecter
                 </button>
