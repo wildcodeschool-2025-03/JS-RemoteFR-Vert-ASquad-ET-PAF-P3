e5befb650e056230a05e7989f20550646d6ff8e0
sunnysmilen <davidsalnicole@gmail.com>
2025-07-17 11:18:01 +0200

    fix: change some div to section and remove console message to toast error and remove spaces

--- client/src/pages/Connexion.tsx
+++ client/src/pages/Connexion.tsx
@@ -80,9 +80,9 @@ export default function Connexion() {
           <input type="submit" value="Connexion" className="button_connexion" />
         </form>
         <p>Tu veux te donner une chance ?</p>
-          <Link to="/signup" className="link_inscription">
-            Rejoins-nous !
-          </Link>
+        <Link to="/signup" className="link_inscription">
+          Rejoins-nous !
+        </Link>
       </section>
     </>
   );
