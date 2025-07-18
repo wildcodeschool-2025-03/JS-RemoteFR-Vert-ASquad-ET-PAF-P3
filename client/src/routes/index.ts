ecf6c0fcd7d6d19ac882745fcd645fae0bc1d2e4
Sadness <85246563+sadnxssdlm@users.noreply.github.com>
2025-07-18 11:51:40 +0200

    Delete useless client/src/routes/index.ts

--- client/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-export const routes = {
-  dashboard: "/dashboard",
-  dashboardCandidate: "/dashboard/candidate",
-  dashboardCompany: "/dashboard/company",
-  dashboardAdmin: "/dashboard/admin",
-  home: "/",
-  offers: "/offers",
-} as const;
