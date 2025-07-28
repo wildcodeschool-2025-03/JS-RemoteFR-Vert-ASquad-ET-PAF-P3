9eb959ce90aa05564410c457b31ded4255959341
sunnysmilen <davidsalnicole@gmail.com>
2025-07-28 11:38:00 +0200

    fix: remove context in app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,13 @@ import { Outlet } from "react-router";
 import "../src/App.css";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
-import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <>
-      <AuthProvider>
         <Navbar />
         <Outlet />
         <Footer />
-      </AuthProvider>
     </>
   );
 }
